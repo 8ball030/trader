4948d568888fc3f63e4222f0dd77d96510cfbb22
8baller <tomrae030@hotmail.com>
2024-06-04 11:17:53 +0000

    mint

--- mint.sh
+++ mint.sh
@@ -6,14 +6,13 @@ function gv(){
 
     rm mints/$2.json || true
     adev metadata generate . $1 $2
-    adev metadata validate mints/$2.json
+    adev -v metadata validate mints/$2.json
 }
 
 
-gv skill/eightballer/ui_loader_abci/0.1.0 12 > /dev/null
-gv custom/eightballer/simple_html/0.1.0 13 > /dev/null
-gv custom/eightballer/simple_react/0.1.0 14 > /dev/null
+# gv skill/eightballer/ui_loader_abci/0.1.0 12 > /dev/null
+# gv custom/eightballer/simple_html/0.1.0 13 > /dev/null
+# gv custom/eightballer/simple_react/0.1.0 14 > /dev/null
 
-gv agent/valory/trader/0.1.0 15
-
-
+# gv agent/eightballer/trader/0.1.0 15
+gv service/eightballer/trader/0.1.0 16
